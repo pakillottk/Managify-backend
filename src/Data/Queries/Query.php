560597258a21d6488eb6908fa714f4e4286ffbf1
Francisco Gázquez <pmgmsc@gmail.com>
2018-01-20 21:51:41 +0100

    Improved Query commentary

--- src/Data/Queries/Query.php
+++ src/Data/Queries/Query.php
@@ -3,12 +3,26 @@
 namespace App\Data\Queries;
 
 class Query {
+    //Format: [ [ field => ... , operator => ... , value => ...  ], ... ]
+    //Result: The WHERE fragment of the select will have these configuration
     protected $select = [];
+    //Format: [ field1, field2... ]
+    //Result: The specified fields won't be returned
     protected $hidden = [];
+    //Format: [ field1, field2... ]
+    //Result: Only the specified fields will be returned
     protected $fields = [];
+    //Format: [ relation1, relation2... ]
+    //Result: Adds the specified relationship to the results
     protected $include = [];
+    //Format: any int [0-MAX_INT]
+    //Result: Sets the current page of the query
     protected $page = 0;
+    //Format: [ [ field => ... , sorting => desc|asc ], ... ]
+    //Result: Sets the order of the query results
     protected $orderBy;
+    //Format: [ operation => field, ... ]
+    //Result: Gets the results of the specified operation on the selected field (affected by select clauses) 
     protected $aggregate = [];
 
     public function __construct( 
